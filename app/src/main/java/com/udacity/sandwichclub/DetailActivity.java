f61fbc549fed8a19ebb53d295d140a73a4f40ebf
Kevin Heath High <kevin@hobbitsoft.net>
2018-04-27 11:31:06 -0700

    Submission to be Reviewed

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.udacity.sandwichclub;
 
+
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -9,16 +11,23 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    //Create the binding variable
+    ActivityDetailBinding mDetailBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Create the binding to the View
+        mDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
@@ -37,13 +46,16 @@ public class DetailActivity extends AppCompatActivity {
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
+        //Update populateUI with Sandwich class parameter
+        populateUI(sandwich);
+
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +68,24 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    //Added Sandwich class parameter
+    private void populateUI(Sandwich sandwich) {
+        //Use Binding to update DetailActivity views from passed Sandwich Class
+        mDetailBinding.descriptionTv.setText(sandwich.getDescription());
+        mDetailBinding.originTv.setText(sandwich.getPlaceOfOrigin());
 
+        //Want each ingredient to be on it's own line (more readable)
+        String ingredients = "";
+        for (String ingredient : sandwich.getIngredients()) {
+            ingredients += ingredient + "\n";
+        }
+        mDetailBinding.ingredientsTv.setText(ingredients);
+
+        //Want each alias to be on it's own line (more readable)
+        String aliases = "";
+        for (String alias : sandwich.getAlsoKnownAs()) {
+            aliases += alias + "\n";
+        }
+        mDetailBinding.alsoKnownTv.setText(aliases);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,61 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    /*  View of JSON Object
+     {
+        "name":{
+            "mainName":"Vada Pav",
+            "alsoKnownAs":["Bombay Burger", "Vada Pao", "Wada Pav", "Wada Pao", "Pao Vada", "Pav Vada", "Pao Wada", "Pav Wada"]
+        },
+        "placeOfOrigin":"India",
+        "description":"Vada Pav is a vegetarian fast food dish native to the Indian state of Maharashtra. The dish consists of a deep fried potato dumpling placed inside a bread bun (pav) sliced almost in half through the middle. It is generally accompanied with one or more chutneys and a green chilli pepper. It originated as cheap street food in Mumbai, but is now served in food stalls and restaurants across India. It is also called Bombay Burger in keeping with its origins and its resemblance in physical form to a burger.",
+        "image":"https://upload.wikimedia.org/wikipedia/commons/1/15/Vada_Paav-The_Mumbai_Burger.jpg",
+        "ingredients": ["Deep-fried mashed potato patty", "chilli peppers", "garlic", "ginger", "bread bun"]
+    }
+    */
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = new Sandwich();
+
+        //Parse the JSON String
+        try {
+            JSONObject sandwichInfo = new JSONObject(json);
+            //Get the Nested "name" JSON
+            JSONObject sandwichName = sandwichInfo.getJSONObject("name");
+            sandwich.setMainName(sandwichName.getString("mainName"));
+            JSONArray alsoKnownAs = sandwichName.getJSONArray("alsoKnownAs");
+            //build List from JSONArray
+            List<String> aliasesList = new ArrayList<String>();
+            for (int i = 0; i < alsoKnownAs.length(); i++) {
+                aliasesList.add(alsoKnownAs.getString(i));
+            }
+            sandwich.setAlsoKnownAs(aliasesList);
+
+            //build List from JSONArray
+            JSONArray sandwichIngredients = sandwichInfo.getJSONArray("ingredients");
+            List<String> ingredientsList = new ArrayList<String>();
+            for (int i = 0; i < sandwichIngredients.length(); i++) {
+                ingredientsList.add(sandwichIngredients.getString(i));
+            }
+            sandwich.setIngredients(ingredientsList);
+
+            sandwich.setPlaceOfOrigin(sandwichInfo.getString("placeOfOrigin"));
+            sandwich.setDescription(sandwichInfo.getString("description"));
+            sandwich.setImage(sandwichInfo.getString("image"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
     }
 }
